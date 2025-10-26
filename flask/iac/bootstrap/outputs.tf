c8957cb1cc5a6ada1b7902b22210c8638f834062
caian23 <caian23@outlook.com>
2025-10-26 15:48:14 -0300

    refactor: fmt terraform files

--- flask/iac/bootstrap/outputs.tf
+++ flask/iac/bootstrap/outputs.tf
@@ -5,3 +5,7 @@ output "bucket_tfstate" {
 output "ecr_url" {
   value = aws_ecr_repository.ecr_repository.repository_url
 }
+
+output "ecr_role" {
+  value = aws_iam_role.ecr_role.arn
+}
--- flask/iac/main/rds.tf
+++ flask/iac/main/rds.tf
@@ -1,5 +1,5 @@
 resource "aws_db_subnet_group" "db_subnet_group" {
-  name = "db_subnet_group"
+  name       = "db_subnet_group"
   subnet_ids = module.vpc.subnet_ids
 
   tags = {
